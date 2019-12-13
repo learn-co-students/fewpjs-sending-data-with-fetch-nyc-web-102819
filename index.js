974402ad19c3bc2b6bf324751879ee75be9e06cd
joffitzer <joffitzer@gmail.com>
2019-12-12 20:46:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Add your code here
+// // Add your code here
+
+// submitData (userName, userEmail) {
+//     return fetch ('http://localhost:3000/users', {
+//         method: 'POST',
+//         headers: {
+//             "Content-Type": "application/json",
+//             "Accept": "application/json"
+//         },
+//         body: JSON.stringify( {
+//             userName,
+//             userEmail
+//         })
+//     })
+//     .then (function (resp) {
+//         return resp.json();
+//     })
+//     .then (function (object) {
+//         document.body.innerHTML = object["id"];
+//     })
+//     .catch (function (error) {
+//         document.body.innerHTML = error.message
+//     })
+// }
+
+function submitData( name, email ) {
+    return fetch( 'http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify( {
+          name,
+          email
+        } )
+      } )
+      .then( function ( response ) {
+        return response.json()
+      } )
+      .then( function ( object ) {
+        document.body.innerHTML = object[ "id" ]
+      } )
+      .catch( function ( error ) {
+        document.body.innerHTML = error.message
+      } )
+  }
