303e800a88dc922fc9062159931045c9111cff22
hasibulc <hasibulc91@gmail.com>
2019-12-16 14:31:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Add your code here
+
+const destinationURL = 'http://localhost:3000/users'
+
+function submitData(name, email) {
+    return fetch(destinationURL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+         "Accept": "application/json"
+        },
+        body: JSON.stringify({name, email})
+    })
+    .then(resp => resp.json())
+    .then(user => document.body.innerHTML = user["id"])
+    .catch(error => document.body.innerHTML = error.message)
+}
